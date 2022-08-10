68cfd53da17720148d146f10622f5337fdf9485a
Matias Senger <m.senger@hotmail.com>
2022-08-10 08:54:08 +0200

    Implemented the creation of a new table containing non repeated index entries

--- huge_dataframe/SQLiteDataFrame.py
+++ huge_dataframe/SQLiteDataFrame.py
@@ -7,6 +7,7 @@ import time
 
 NAME_OF_TABLE_IN_SQLITE_DATABASE = 'dataframe_table'
 NAME_OF_INDEX_IN_SQLITE_DATABASE = 'dataframe_index'
+NAME_OF_TABLE_DATAFRAME_INDEX_AS_SET = 'dataframe_index_without_duplicates'
 
 class SQLiteDataFrameDumper:
 	
@@ -80,14 +81,23 @@ class SQLiteDataFrameDumper:
 		"""
 		if hasattr(self, '_list_of_dataframes') and len(self._list_of_dataframes) > 0:
 			df = pandas.concat(self._list_of_dataframes)
+			save_index = list(self._original_dataframe.index.names) != [None]
 			with warnings.catch_warnings():
 				warnings.filterwarnings("ignore", message="The spaces in these column names will not be changed. In pandas versions < 0.14, spaces were converted to underscores.")
 				df.to_sql(
 					NAME_OF_TABLE_IN_SQLITE_DATABASE, 
 					self.sqlite_connection, 
 					if_exists = 'append',
-					index = list(self._original_dataframe.index.names) != [None], # If there is an index by the user, use it, otherwise don't use it.
+					index = save_index,
 				)
+				if save_index:
+					df.index.to_frame(index=False).to_sql(
+						NAME_OF_TABLE_DATAFRAME_INDEX_AS_SET,
+						self.sqlite_connection,
+						if_exists = 'append',
+						index = False,
+					)
+					self._remove_duplicates_from_table(NAME_OF_TABLE_DATAFRAME_INDEX_AS_SET)
 		self._list_of_dataframes = list()
 		self._n_appends_since_last_dump = 0
 		self._time_when_last_dump = time.time()
@@ -109,15 +119,23 @@ class SQLiteDataFrameDumper:
 				index_columns = index_columns[:-2]
 				self.sqlite_connection.cursor().execute(f'CREATE INDEX "{NAME_OF_INDEX_IN_SQLITE_DATABASE}" ON "{NAME_OF_TABLE_IN_SQLITE_DATABASE}" ({index_columns});')
 				self.sqlite_connection.cursor().execute(f'DROP INDEX {name_of_index_to_rename};')
-		
+				
+				self._remove_duplicates_from_table(NAME_OF_TABLE_DATAFRAME_INDEX_AS_SET)
+				
 		self.sqlite_connection.close()
+	
+	def _remove_duplicates_from_table(self, table_name:str):
+		"""Remove duplicate entries from a table."""
+		self.sqlite_connection.cursor().execute(f'CREATE TABLE {table_name}_delete_me_table as SELECT DISTINCT * FROM {table_name}')
+		self.sqlite_connection.cursor().execute(f'DROP TABLE {table_name}')
+		self.sqlite_connection.cursor().execute(f'ALTER TABLE {table_name}_delete_me_table RENAME TO {table_name}')
 
 def load_whole_dataframe(path_to_sqlite_file:Path):
 	"""Loads the whole dataframe stored in the SQLite file into memory 
 	(make sure it fits!). 
 	
-	Parameters
-	----------
+	Arguments
+	---------
 	path_to_sqlite_file: Path
 		Path to a file produced by `SQLiteDataFrameDumper`.
 	"""
@@ -130,3 +148,17 @@ def load_whole_dataframe(path_to_sqlite_file:Path):
 	names_of_index_columns = list(pandas.read_sql(f'PRAGMA index_info({index_name});', connection)['name'])
 	df.set_index(names_of_index_columns, inplace=True)
 	return df
+
+def load_only_index_without_repeated_entries(path_to_sqlite_file:Path):
+	"""If the file was created with an index, this function returns such
+	index as a dataframe, without repeated entries. Thus, it will be much
+	easier for it to fit in memory. You can then use each row of if to
+	access to each index entry in the sqlite file.
+	
+	Arguments
+	---------
+	path_to_sqlite_file: Path
+		Path to a file produced by `SQLiteDataFrameDumper`.
+	"""
+	connection = sqlite3.connect(path_to_sqlite_file)
+	return pandas.read_sql(f'SELECT * from {NAME_OF_TABLE_DATAFRAME_INDEX_AS_SET}', connection)
